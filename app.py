1ac4753019d9711e923f6a2e40277a145443d29f
Aditya Tiwari <112470339+Dxpher@users.noreply.github.com>
2023-12-13 21:10:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,20 +13,23 @@ def main():
     num1 = st.text_input("Enter the first number:", value="")
     num2 = st.text_input("Enter the second number:", value="")
     num3 = st.text_input("Enter the third number:", value="")
+    
+
     if st.button("Find Largest"):
         if not num1 or not num2 or not num3:
             st.warning("Please enter values for all three numbers.")
             return
 
         try:
+  
             num1 = float(num1)
             num2 = float(num2)
             num3 = float(num3)
 
             largest = find_largest(num1, num2, num3)
             st.success(f"The largest number is: {largest}")
-        except ValueError:
-            st.error("Please enter valid numeric values.")
+        except ValueError as e:
+            st.error(f"Please enter valid numeric values. Error: {e}")
             return
 
 if __name__ == "__main__":
