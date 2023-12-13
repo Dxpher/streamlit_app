5fdb32e07a4a785feb0ed7c439a3ac54c2ac6921
Aditya Tiwari <112470339+Dxpher@users.noreply.github.com>
2023-12-13 21:08:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,15 @@ def find_largest(num1, num2, num3):
 
 def main():
     st.title("Find the Largest Number")
-    
-    # User input for three numbers using text_input
     num1 = st.text_input("Enter the first number:", value="")
     num2 = st.text_input("Enter the second number:", value="")
     num3 = st.text_input("Enter the third number:", value="")
-    
-    # Button to find the largest number
     if st.button("Find Largest"):
         if not num1 or not num2 or not num3:
             st.warning("Please enter values for all three numbers.")
             return
 
         try:
-            # Convert input to float
             num1 = float(num1)
             num2 = float(num2)
             num3 = float(num3)
